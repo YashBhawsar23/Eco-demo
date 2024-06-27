891f13aef052f501f85ea2a1bc07ab645a2c8dea
Yash Bhawsar <yashbhawsar97@gmail.com>
2024-06-27 10:24:35 +0530

    second Commit

---
+++ frontend/src/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: "1",
+    name: "Swaggin Dope | Premium White Tshirt",
+    image: "/images/tshirt1.JPG",
+    description:
+      "Experience the perfect blend of comfort and style with our Premium White T-Shirt. Made from 100% pure cotton with a robust 220 GSM fabric weight, this tee offers a luxurious feel and lasting durability. Ideal for any occasion, it ensures you stay cool and comfortable all day long.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 849,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Swaggin Panther | Premium Grey Tshirt",
+    image: "/images/tshirt2.JPG",
+    description:
+      "Upgrade your basics with our Premium Grey T-Shirt. Made from 100% pure cotton with a robust 220 GSM fabric weight, this tee offers unmatched comfort and durability. Perfect for any occasion, it combines classic simplicity with superior quality.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 849,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "3",
+    name: "Swaggin Spring | Premium Pink Tshirt",
+    image: "/images/tshirt3.JPG",
+    description:
+      "Add a pop of color to your wardrobe with our Premium Pink T-Shirt. Made from 100% pure cotton with a sturdy 220 GSM fabric weight, this tee offers unbeatable comfort and durability. Perfect for any occasion, it ensures you look stylish while feeling great all day long.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 849,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Swaggin Nirvana | Premium Black Tshirt",
+    image: "./images/tshirt4.JPG",
+    description:
+      "Elevate your wardrobe with our Premium Black T-Shirt, crafted from 100% pure cotton with a substantial 220 GSM fabric weight. This tee combines luxurious comfort and superior durability, making it the perfect choice for any occasion.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 849,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+export default products;
